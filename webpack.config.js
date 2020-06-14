6c93bd63cdc5def81c83808f3d4c35f3ad2fde11
allyoucanmap <bovio.stefano@gmail.com>
2020-06-14 22:07:14 +0200

    change output bundle

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,8 @@ module.exports = {
     },
     output: {
         filename: '[name].js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        library: '[name]',
+        libraryTarget: 'umd'
     }
 };
